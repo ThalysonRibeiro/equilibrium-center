5d33a2fc52675203b8d24be63799fc9e989add47
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-05-06 17:24:30 -0300

    feat: create card reminders

---
+++ src/app/(panel)/dashboard/_components/reminder/reminder-list.tsx
@@ -0,0 +1,89 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Reminder } from "@/generated/prisma"
+import { Plus, Trash } from "lucide-react"
+import { deleteReminder } from "../../_actions/delete-reminder"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { ReminderContent } from "./reminder-content"
+import { useState } from "react"
+
+interface ReminderListProps {
+  reminder: Reminder[];
+}
+
+export function ReminderList({ reminder }: ReminderListProps) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const router = useRouter();
+
+  async function handleDeleteReminder(id: string) {
+    const response = await deleteReminder({ reminderId: id });
+
+    if (response.error) {
+      toast.error(response.error);
+      return;
+    }
+    toast.success(response.data);
+    router.refresh();
+  }
+
+  return (
+    <div className="flex flex-col gap-3">
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-lx md:text-2xl font-bold">
+            Lembretes
+          </CardTitle>
+
+          <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <DialogTrigger asChild>
+              <Button className="w-9 p-0 bg-corsecondary hover:bg-corprimary">
+                <Plus className="w-5 -h-5" />
+              </Button>
+            </DialogTrigger>
+
+            <DialogContent className="sm:max-w-[425px] border border-corprimary">
+              <DialogHeader>
+                <DialogTitle>Novo Lembrete</DialogTitle>
+                <DialogDescription>Criar um novo lemrete para sua lista.</DialogDescription>
+              </DialogHeader>
+
+              <ReminderContent
+                closeDialog={() => setIsDialogOpen(false)}
+              />
+            </DialogContent>
+          </Dialog>
+
+        </CardHeader>
+
+        <CardContent>
+          {reminder.length === 0 && (
+            <p className="text-sm text-gray-500">
+              Nenhum lembrete resgistrado...
+            </p>
+          )}
+          <ScrollArea className="h-[340px] lg:max-h-[cal(100vh-15rem)] pr-0 w-full flex-1">
+            {reminder.map((item) => (
+              <article key={item.id} className="flex flex-wrap flex-row items-center justify-between py-1 bg-yellow-100 mb-2 px-3 rounded-md">
+                <p className="text-sm lg:text-base">
+                  {item.description}
+                </p>
+                <Button
+                  className="bg-red-500 hover:bg-red-400 shadow-none rounded-full p-2"
+                  size="sm"
+                  onClick={() => handleDeleteReminder(item.id)}
+                >
+                  <Trash className="w-4 h-4" />
+                </Button>
+              </article>
+            ))}
+          </ScrollArea>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
 import getSession from "@/lib/getSession";
+import { Calendar } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
+import { ButtonCopyLink } from "./_components/button-copy-link";
+import { Reminders } from "./_components/reminder/reminders";
 
 export default async function Dashboard() {
   const session = await getSession();
@@ -9,8 +14,22 @@ export default async function Dashboard() {
   }
 
   return (
-    <div>
-      Dashboard
-    </div>
+    <main>
+      <div className="space-x-2 flex items-center justify-end">
+        <Link href={`/clinica/${session.user.id}`} target="_blank">
+          <Button
+            className="bg-corsecondary hover:bg-corprimary flex-1 md:flex-[0]"
+          >
+            <Calendar />
+            <span>Novo agendamento</span>
+          </Button>
+        </Link>
+        <ButtonCopyLink userId={session.user.id!} />
+      </div>
+      <section className="grid grid-cols-1 gap-4 lg:grid-cols-2 mt-4">
+        <div>agenda</div>
+        <Reminders userId={session.user.id!} />
+      </section>
+    </main>
   );
 }
