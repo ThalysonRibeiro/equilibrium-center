ba8e2430df06afb52942b0354152ade391f6b089
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-05-08 18:16:35 -0300

    feat: functionality to list available clinics

--- src/app/(panel)/dashboard/_components/appointments/dialog-appointments.tsx
+++ src/app/(panel)/dashboard/_components/appointments/dialog-appointments.tsx
@@ -13,10 +13,10 @@ export function DialogAppointments({ appointment }: DialogAppointmentsProps) {
   return (
     <DialogContent className="border-corprimary">
       <DialogHeader>
-        <DialogTitle>
+        <DialogTitle className="text-center text-corprimary">
           Detalhes do agendamento
         </DialogTitle>
-        <DialogDescription>
+        <DialogDescription className="text-center">
           Veja todos os detalhes do agendamento
         </DialogDescription>
       </DialogHeader>
@@ -24,15 +24,21 @@ export function DialogAppointments({ appointment }: DialogAppointmentsProps) {
       <div className="py-4">
         {appointment && (
           <article>
-            <p><span className="font-bold">Horario agendado: </span>{appointment.time}</p>
-            <p className="mb-2"><span className="font-bold">Data do agendamento: </span>{format(appointment.appointmentDate, "dd/MM/yyyy")}</p>
-            <p><span className="font-bold">Nome: </span>{appointment.name}</p>
-            <p><span className="font-bold">Telefone: </span>{appointment.phone}</p>
-            <p><span className="font-bold">Email: </span>{appointment.email}</p>
+            <p><span className="font-bold text-corprimary">Horario agendado: </span>{appointment.time}</p>
+            <p className="mb-2"><span className="font-bold text-corprimary">Data do agendamento: </span>
+              {new Intl.DateTimeFormat('pt-BR', {
+                timeZone: "UTC",
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+              }).format(new Date(appointment.appointmentDate))}</p>
+            <p><span className="font-bold text-corprimary">Nome: </span>{appointment.name}</p>
+            <p><span className="font-bold text-corprimary">Telefone: </span>{appointment.phone}</p>
+            <p><span className="font-bold text-corprimary">Email: </span>{appointment.email}</p>
 
-            <section className="bg-corsecondary mt-4 p-2 rounded-md">
-              <p><span className="font-bold">Serviço: </span>{appointment.service.name}</p>
-              <p><span className="font-bold">Valor: </span>{formatCurrency(appointment.service.price.toString())}</p>
+            <section className="bg-corsecondary border border-corprimary/50 mt-4 p-2 rounded-md">
+              <p><span className="font-bold text-corprimary">Serviço: </span>{appointment.service.name}</p>
+              <p><span className="font-bold text-corprimary">Valor: </span>{formatCurrency(appointment.service.price.toString())}</p>
             </section>
 
           </article>
