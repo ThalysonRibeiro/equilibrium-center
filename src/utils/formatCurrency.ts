83fb259bb7ffb9eda721863b99e3ef0f5701ee90
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-05-20 23:25:05 -0300

    feat: function to format and abbreviate price

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -8,3 +8,13 @@ export function formatCurrency(value: string) {
   const number = parseFloat(value);
   return CURRENCY_FORMATTER.format(number);
 }
+
+export function formatShortNumber(value: number): string {
+  if (value >= 1_000_000) {
+    return (value / 1_000_000).toFixed(1).replace(/\.0$/, '') + 'M';
+  }
+  if (value >= 1_000) {
+    return (value / 1_000).toFixed(1).replace(/\.0$/, '') + 'k';
+  }
+  return value.toString();
+}
