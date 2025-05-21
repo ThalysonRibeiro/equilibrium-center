b6ace6355ee6585a1483a15b4b946e9f29fa4704
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-05-20 23:27:02 -0300

    feat:created function for service metrics and listing

---
+++ src/app/api/metrics/services/route.ts
@@ -0,0 +1,63 @@
+import { auth } from "@/lib/auth";
+import { NextResponse } from 'next/server';
+import { getAppointments } from "@/lib/prisma/get-appointments";
+import prisma from "@/lib/prisma";
+import { count } from "console";
+
+type ServiceWithCounting = {
+  id: string;
+  name: string;
+  price: string;
+  count: number;
+  totalAmount: number;
+  totalDuration: number;
+};
+
+export const GET = auth(async function GET(req) {
+  if (!req.auth) {
+    return NextResponse.json({ error: "Acesso não autorizado" }, { status: 401 });
+  }
+
+  const clinicId = req.auth?.user?.id;
+  if (!clinicId) {
+    return NextResponse.json({ error: "Usuário não encontrado!" }, { status: 400 });
+  }
+
+
+  const appointments = await getAppointments({
+    userId: clinicId,
+    // startDate,
+    // endDate,
+    status: 'COMPLETED',
+  });
+
+  const map = new Map<string, ServiceWithCounting>();
+
+  appointments.forEach(({ service }) => {
+    const existing = map.get(service.id);
+    if (existing) {
+      existing.count++;
+      existing.totalAmount += service.price.toNumber();
+      existing.totalDuration += service.duration;
+    } else {
+      map.set(service.id, {
+        id: service.id,
+        name: service.name,
+        price: service.price.toString(),
+        count: 1,
+        totalAmount: service.price.toNumber(),
+        totalDuration: service.duration
+      });
+    }
+  });
+
+  const topService = Array.from(map.values()).sort((a, b) => b.count - a.count);
+
+
+
+  return NextResponse.json({
+    topService
+    // allServices,
+    // appointments
+  })
+})
