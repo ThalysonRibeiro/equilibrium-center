5cc424a56ecc43c6a6e83ae0996fbcb560d14d6b
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-05-21 23:53:49 -0300

    feat: created function for create object  groupAppointmentsByDate

---
+++ src/utils/group-appointments-by-date.ts
@@ -0,0 +1,53 @@
+
+export type DataTimes = {
+  appointmentDate?: Date;
+  count: number;
+  daysOfWeek: string;
+};
+
+const daysOfWeek = [
+  'domingo',
+  'segunda-feira',
+  'terça-feira',
+  'quarta-feira',
+  'quinta-feira',
+  'sexta-feira',
+  'sábado'
+];
+
+
+export function groupAppointmentsByDate(
+  appointments: { appointmentDate?: Date | string }[],
+  weekStart: Date
+): Map<string, DataTimes> {
+
+  const map = new Map<string, DataTimes>();
+
+  for (let i = 0; i < 7; i++) {
+    const current = new Date(weekStart);
+    current.setDate(weekStart.getDate() + i);
+    const dateKey = current.toISOString().split("T")[0];
+
+    const dayIndex = current.getDay(); // Corrige a correspondência do dia da semana
+
+    map.set(dateKey, {
+      appointmentDate: current,
+      daysOfWeek: daysOfWeek[dayIndex],
+      count: 0,
+    });
+  }
+
+  appointments.forEach(({ appointmentDate }) => {
+    if (!appointmentDate) return;
+
+    const date = new Date(appointmentDate);
+    const dateKey = date.toISOString().split("T")[0];
+
+    const existing = map.get(dateKey);
+    if (existing) {
+      existing.count++;
+    }
+  });
+
+  return map;
+}
