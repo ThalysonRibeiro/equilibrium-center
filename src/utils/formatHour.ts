d53d6780b82e5bedd7ae1763b66be1dbcf376f9f
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-05-23 13:51:17 -0300

    feat:new function format duration

--- src/utils/formatHour.ts
+++ src/utils/formatHour.ts
@@ -3,3 +3,14 @@ export function FormatHour(value: number) {
   const minutes = (value % 60).toString().padStart(2, '0');
   return `${hour}:${minutes}`
 }
+
+export const formatDuration = (minutes: number): string => {
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = minutes % 60;
+
+  if (hours > 0) {
+    return `${hours}h ${remainingMinutes > 0 ? `${remainingMinutes}m` : ''}`;
+  }
+
+  return `${minutes}m`;
+};
