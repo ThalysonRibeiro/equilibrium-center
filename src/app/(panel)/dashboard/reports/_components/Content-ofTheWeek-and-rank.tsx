2869f525ab36ecc4b4bb453993d3bb7282db0790
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-05-22 21:09:42 -0300

    refactor component and created TopCustomers

---
+++ src/app/(panel)/dashboard/reports/_components/Content-ofTheWeek-and-rank.tsx
@@ -0,0 +1,57 @@
+"use client"
+import { useQuery } from "@tanstack/react-query";
+import { BarChartLastOfWeek } from "./bar-chart-lastOfWeek";
+import { TopHours } from "./top-hours";
+import { fetchData } from "@/utils/fetch-data";
+import { WeeklySummaryProps } from "../types/weekly-summary";
+import { LoadingUI } from "@/components/ui/loading-ui";
+import { RanksProps } from "../types/ranks";
+import { TopCustomers } from "./top-customers";
+import { createQueryFetcher } from "@/utils/createQueryFetcher";
+
+export function ContentOfTheWeekAndRank() {
+
+  const {
+    data: weeklySummary,
+    isLoading: isLoadingWeeklySummary,
+    refetch: refetchWeeklySummary,
+  } = useQuery({
+    queryKey: ["get-metrics-weekly-summary"],
+    queryFn: createQueryFetcher<WeeklySummaryProps>("metrics/weekly-summary"),
+    staleTime: 80000,
+    refetchInterval: 90000,
+  });
+
+  const {
+    data: topCustomersAndHours,
+    isLoading: isLoadingTopCustomersAndHours,
+    refetch: refetchTopCustomersAndHours,
+  } = useQuery({
+    queryKey: ["get-metrics-rank"],
+    queryFn: createQueryFetcher<RanksProps>("metrics/ranks"),
+    staleTime: 80000,
+    refetchInterval: 90000,
+  });
+
+  if (isLoadingWeeklySummary && isLoadingTopCustomersAndHours) {
+    return (
+      <LoadingUI />
+    )
+  }
+
+  return (
+    <section className="space-y-4 w-full mb-4">
+      {weeklySummary && (
+        <>
+          <BarChartLastOfWeek data={weeklySummary} />
+        </>
+      )}
+      {topCustomersAndHours && (
+        <div className="flex gap-4 flex-col xl:flex-row">
+          <TopCustomers data={topCustomersAndHours} />
+          <TopHours data={topCustomersAndHours} />
+        </div>
+      )}
+    </section>
+  )
+}
