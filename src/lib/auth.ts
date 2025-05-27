92fc732d7300cc6ba924496708572784a59766d0
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-05-26 22:50:02 -0300

    feat:create login in google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,10 +4,12 @@ import prisma from "./prisma"
 import { Adapter } from "next-auth/adapters";
 import GitHub from "next-auth/providers/github"
 import Discord from "next-auth/providers/discord"
+import Google from "next-auth/providers/google"
+
 
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma) as Adapter,
   trustHost: true,
-  providers: [GitHub, Discord],
+  providers: [GitHub, Discord, Google],
 })
