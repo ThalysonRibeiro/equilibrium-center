7f6029a179b9ebd4236ed4a792ee6c8172d5a8fe
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-05-26 22:41:37 -0300

    feat: label trial

--- src/app/(panel)/dashboard/layout.tsx
+++ src/app/(panel)/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import { SidebarDashboard } from "./_components/sidebar";
 import getSession from "@/lib/getSession";
 import { Toaster } from "sonner";
 import { checkSubscription } from "@/utils/permissions/checkSubscription";
+import { LabelSubscription } from "@/components/ui/label-subscription";
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
   const session = await getSession();
@@ -18,6 +19,16 @@ export default async function DashboardLayout({ children }: { children: React.Re
           position="top-right"
           richColors
         />
+        {permission?.subscriptionStatus === "EXPIRED" && (
+          <LabelSubscription expired={true} />
+        )}
+        {permission?.subscriptionStatus === "TRIAL" && (
+          <div className="bg-green-500 text-white text-sm md:text-base px-3 py-4 rounded-md my-4">
+            <p className="font-semibold">
+              {permission.message}
+            </p>
+          </div>
+        )}
         {children}
       </SidebarDashboard>
     </>
