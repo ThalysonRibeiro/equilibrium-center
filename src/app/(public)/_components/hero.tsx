ce3487b4ea853d1622425308c301d38be0553a9a
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-05-27 23:39:36 -0300

    feat:styles, layout

--- src/app/(public)/_components/hero.tsx
+++ src/app/(public)/_components/hero.tsx
@@ -1,60 +1,74 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import img_bg_hero from "@/assets/bg-3.png";
+
 import img_hero from "@/assets/img-hero.jpg";
+import { BgHero } from "./bg-hero";
+import { EDIT_COOLDOWN_DAYS } from "@/app/(panel)/dashboard/profile/edit-cooldwn-days";
+import { Check, CheckCircle, CheckCircle2 } from "lucide-react";
 
 export function Hero() {
   return (
     <section className="relative w-full h-175">
-      <div className=" w-full absolute h-175 -z-10">
-        <Image
-          src={img_bg_hero}
-          alt="imagem do hero inlustratica"
-          fill
-          className="object-cover"
-        />
-      </div>
-      <div
-        className="container mx-auto sm:px-1 min-h-150 px-6"
-      >
+      <BgHero />
 
-        <main className="flex flex-col lg:flex-row items-center justify-center w-full h-full gap-6 sm:pt-40 pt-20">
+      <div className="container mx-auto sm:px-1 min-h-150 px-6">
 
-          <article className="z-10 pt-5 space-y-5 max-w-3xl w-full flex flex-col justify-center">
-            <h1 className="text-corprimary text-7xl font-bold font-mansalva text-shadow-md">
-              Equilibrium Center
+        <main className="flex flex-col lg:flex-row items-center justify-between px-6 w-full h-full gap-6 sm:pt-40 pt-20">
+
+          <article className="text-primary z-10 pt-5 space-y-5 max-w-3xl lg:w-1/2 w-full flex flex-col justify-center">
+            <h1 className="text-shadow-md font-montserrat">
+              <span className="text-ring font-semibold text-6xl">
+                Otimize sua prática
+              </span>
+              <br className="md:block hidden" />
+              <span className="font-semibold text-6xl lg:ml-0 ml-2">
+                de massoterapia
+              </span>
             </h1>
-            <p className="text-corsecondary font-medium text-3xl">
-              Bem-estar
+
+            <p className="text-xl">
+              A plataforma de gestão completa projetada especificamente para massoterapeutas. Agende consultas, gerencie clientes e expanda seus negócios com facilidade
             </p>
-            <p className="text-corsecondary font-medium text-2xl">
-              Saudável com perfeição
-            </p>
-            <p className="text-corsecondary max-w-140 text-sm">
-              Mente e bem-estar. Mais do que um hub de clínicas, somos um centro integrativo que reúne profissionais qualificados em massoterapia, terapias complementares e cuidados corporais.
-              Com ambientes acolhedores, atendimento humanizado e foco na saúde integral, oferecemos um ecossistema de cuidados personalizados para quem busca qualidade de vida, relaxamento e reabilitação.
-            </p>
-            <Button
-              className="bg-corsecondary hover:bg-corprimary w-fit"
+
+            <div
+              className="p-4 bg-ring rounded-3xl w-fit  text-white font-semibold shadow-md hover:shadow-lg"
             >
-              Comece gratis
-            </Button>
-          </article>
-
-          <article className="hidden lg:block relative">
-
-            <div className="absolute right-105 bottom-50 z-10 w-50 h-50 bg-white text-corsecondary font-bold border-15 border-corprimary rounded-full flex items-center justify-center text-center text-2xl p-4 capitalize">
-              up to 50% off all package
+              Comece seu test gratuito de {EDIT_COOLDOWN_DAYS} dias
             </div>
 
-            <div className="relative w-131 h-90 border-15 border-corprimary">
+            <div className="lg:hidden w-fit border border-dashed border-ring rounded-full flex gap-2.5 items-center justify-center text-center font-semibold md:text-xl p-2 uppercase text-primary">
+              <p>50% no primeiro mês</p>
+              <p>use: MAS50OFF</p>
+            </div>
+
+            <div className="flex gap-4 flex-col md:flex-row text-sm">
+              <p className="flex items-center">
+                <Check className="text-white rounded-full p-0.5 w-4 h-4 bg-ring mr-1" />
+                Não é necessário cartão de crédito
+              </p>
+              <p className="flex items-center">
+                <Check className="text-white rounded-full p-0.5 w-4 h-4 bg-ring mr-1" />
+                Cancelar a qualquer momento
+              </p>
+            </div>
+          </article>
+
+          <article className="hidden lg:block relative w-1/2">
+
+            <div className="absolute top-15 left-10 z-10 border border-dashed border-ring rounded-full flex gap-2.5 items-center justify-center text-center font-semibold text-xl p-2 uppercase text-primary bg-white/40 backdrop-blur-xl">
+              <p>50% no primeiro mês</p>
+              <p>use: MAS50OFF</p>
+            </div>
+
+
+            <div className="relative w-full h-125 border-15 border-ring rounded-4xl">
               <Image
                 src={img_hero}
                 alt="imagem do hero inlustratica"
                 fill
                 priority
                 quality={100}
-                className="object-contain"
+                className="object-cover rounded-2xl"
               />
             </div>
           </article>
