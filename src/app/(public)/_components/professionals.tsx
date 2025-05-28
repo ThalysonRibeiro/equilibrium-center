abffb85f6c2f17f13cf5af60b909376c22985717
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-05-27 23:42:15 -0300

    style: adjusted class and porps

--- src/app/(public)/_components/professionals.tsx
+++ src/app/(public)/_components/professionals.tsx
@@ -1,12 +1,14 @@
 import {
   Card,
-  CardContent
+  CardContent,
+  CardFooter
 } from "@/components/ui/card";
 import Image from "next/image";
 import img_test from "@/assets/img-hero.jpg";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
-import img_formas from "@/assets/formas.png";
+import img_form_1 from "@/assets/form-1.png";
+import img_form_2 from "@/assets/form-2.png";
 import { User } from "@/generated/prisma";
 
 interface ProfessionalsProps {
@@ -16,26 +18,34 @@ interface ProfessionalsProps {
 
 export function Professionals({ professionals }: ProfessionalsProps) {
   return (
-    <section className="py-16 relative">
-      <div className=" w-full absolute h-135 -z-10 top-0">
+    <section className="py-6 relative bg-white">
+      <div className=" w-80 h-70 absolute top-0 left-0">
         <Image
-          src={img_formas}
+          src={img_form_1}
+          alt="imagem do hero inlustratica"
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div className=" w-80 h-70 absolute bottom-0 right-0">
+        <Image
+          src={img_form_2}
           alt="imagem do hero inlustratica"
           fill
           className="object-cover"
         />
       </div>
       <div className="container mx-auto px-6 sm:px-1">
-        <h2 className="text-3xl text-center mb-12 text-corprimary font-bold font-mansalva">
-          Clinicas disponivel
+        <h2 className="text-primary text-3xl text-center mb-12 font-bold">
+          Profissionais disponivel
         </h2>
 
         <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 px-6">
           {professionals.map(professional => (
-            <Card key={professional.id} className="overflow-hidden p-0">
+            <Card key={professional.id} className="overflow-hidden pt-0 hover:scale-105 transition-transform duration-300">
               <CardContent className="p-0">
                 <div>
-                  <div className="relative h-48">
+                  <div className="relative h-50">
                     <Image
                       src={professional.image ?? img_test}
                       alt="imagem do profissional"
@@ -45,27 +55,28 @@ export function Professionals({ professionals }: ProfessionalsProps) {
                   </div>
                 </div>
 
-                <div className="p-4 space-y-4 border">
+                <div className="p-4 space-y-4">
                   <div className="flex items-center justify-between">
                     <div>
-                      <h3 className="font-bold">
+                      <h3 className="font-bold capitalize">
                         {professional.name}
                       </h3>
                       <p className="text-sm text-cinza">
-                        {professional.address ?? "Endereço não informado"}
+                        {professional.address ? professional.address : "Endereço não informado"}
                       </p>
                     </div>
-                    <div className="w-3 h-3 rounded-full bg-corprimary" />
                   </div>
-                  <Link
-                    href={`/clinica/${professional.id}`}
-                    target="_blank"
-                    className="bg-corsecondary hover:bg-corprimary w-full text-texto flex items-center justify-center py-2 px-1 rounded-md text-sm font-medium">
-                    Agendar horário
-                    <ArrowRight className="ml-2" />
-                  </Link>
                 </div>
               </CardContent>
+              <CardFooter>
+                <Link
+                  href={`/clinica/${professional.id}`}
+                  target="_blank"
+                  className="bg-ring hover:bg-accent text-white w-full flex items-center justify-center py-2 px-1 rounded-md text-sm font-medium">
+                  Agendar horário
+                  <ArrowRight className="ml-2" />
+                </Link>
+              </CardFooter>
             </Card>
           ))}
         </section>
