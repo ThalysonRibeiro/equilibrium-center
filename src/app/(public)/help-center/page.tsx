920c44dfe96472d6c6a8fe0615a0b73be646c7c0
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-06-03 00:01:18 -0300

    feat: created page HelpCenter

---
+++ src/app/(public)/help-center/page.tsx
@@ -0,0 +1,18 @@
+import { CircleAlert, Hammer } from "lucide-react";
+
+export default function HelpCenter() {
+  return (
+    <main className="container mx-auto px-4 p-6 space-y-4 min-h-screen">
+      <h1 className="text-5xl font-semibold text-center">Centro de ajuda</h1>
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <Hammer className="w-20 h-20" />
+        <div className="flex items-center gap-1">
+          <p className="text-2xl">
+            Página em construção
+          </p>
+          <CircleAlert />
+        </div>
+      </div>
+    </main>
+  )
+}
