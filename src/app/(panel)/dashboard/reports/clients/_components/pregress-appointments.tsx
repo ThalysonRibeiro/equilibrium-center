b5608130d703cde3b6cbac7158c57d641b6a9c09
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-06-03 15:41:13 -0300

    feat:change name page and router

---
+++ src/app/(panel)/dashboard/reports/clients/_components/pregress-appointments.tsx
@@ -0,0 +1,103 @@
+"use client"
+import { Activity } from "lucide-react";
+import {
+  Card,
+  CardContent, CardHeader,
+  CardTitle
+} from "@/components/ui/card";
+import { MetricStatusProps } from "../../types/allApponitments";
+import { RadialProgress } from "@/components/radial-progress";
+import { LoadingUI } from "@/components/ui/loading-ui";
+
+interface ProgressAppointmentsProps {
+  metricStatus: MetricStatusProps | null;
+  countAllAppointments: number | 0;
+  loading: boolean;
+}
+
+export function ProgressAppointments({ metricStatus, countAllAppointments, loading }: ProgressAppointmentsProps) {
+  return (
+    <>
+      {loading ? (
+        <div className="w-full h-full flex justify-center items-center">
+          <LoadingUI />
+        </div>
+      ) : (
+        <article>
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-3">
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-primary text-sm inline-flex justify-between font-montserrat">
+                  Total de<br />
+                  Agendamentos
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex items-center justify-between">
+                <span className="text-5xl font-montserrat">{countAllAppointments}</span>
+                <Activity className="w-8 h-8" />
+              </CardContent>
+            </Card>
+            <CardMetricsStatus
+              title={"Completo"}
+              countValue={metricStatus?.countByStatus.countCompleted || 0}
+              percentage={metricStatus?.percentageByStatus.countCompleted || "0%"}
+              width={metricStatus?.countByStatus.countCompleted || 0}
+              max={countAllAppointments}
+              color="bg-green-500"
+              colorRadial="oklch(72.3% 0.219 149.579)"
+            />
+            <CardMetricsStatus
+              title={"Não comparecido"}
+              countValue={metricStatus?.countByStatus.countNo_show || 0}
+              percentage={metricStatus?.percentageByStatus.countNo_show || "0%"}
+              width={metricStatus?.countByStatus.countNo_show || 0}
+              max={countAllAppointments}
+              color="bg-orange-500"
+              colorRadial="oklch(70.5% 0.213 47.604)"
+            />
+            <CardMetricsStatus
+              title={"Cancelado"}
+              countValue={metricStatus?.countByStatus.countCancelled || 0}
+              percentage={metricStatus?.percentageByStatus.countCancelled || "0"}
+              width={metricStatus?.countByStatus.countCancelled || 0}
+              max={countAllAppointments}
+              color="bg-red-500"
+              colorRadial="oklch(63.7% 0.237 25.331)"
+            />
+
+          </div>
+        </article>
+      )}
+    </>
+  )
+}
+
+interface CardMetricsStatusProps {
+  title: string,
+  countValue: number,
+  percentage: string;
+  width: number,
+  max: number,
+  color: string,
+  colorRadial: string,
+}
+
+function CardMetricsStatus({ title, countValue, percentage, width, max, color, colorRadial }: CardMetricsStatusProps) {
+  return (
+    <Card className="text-sm text-primary relative">
+      <CardHeader>
+        <div className={`absolute top-0 left-0 ${color} w-2 h-full rounded-l-lg`}></div>
+        <CardTitle>
+          {title}
+          <p className="font-montserrat">Total: <span className="text-2xl">{countValue}</span></p>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col items-center justify-center">
+        <RadialProgress
+          progress={Number(percentage.replace("%", ""))}
+          color={colorRadial}
+        />
+      </CardContent>
+    </Card>
+  )
+}
